0134b9e80a1cbc9c41ad5be107f3db23319100a2
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-09 16:53:07 +0900

    19. Cookieを使う

--- basic_php_webdev-master/work/web/result.php
+++ basic_php_webdev-master/work/web/result.php
@@ -2,13 +2,14 @@
 
 require('../app/functions.php');
 
-$color = filter_input(INPUT_GET, 'color') ?? 'transparent';
+$colorFromGet = filter_input(INPUT_GET, 'color') ?? 'transparent';
+setcookie('color', $colorFromGet);
 
 include('../app/_parts/_header.php');
 
 ?>
 
-<p><?= h($color); ?></p>
+<p><?= h($colorFromGet); ?></p>
 <p><a href="index.php">Go Back</a></p>
 
 <?php
