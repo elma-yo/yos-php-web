b72e73ced860adb088701ad13e2733a1819e35c0
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-09 18:15:22 +0900

    29. POSTされたかどうか調べる

--- basic_php_webdev-master/work/web/result.php
+++ basic_php_webdev-master/work/web/result.php
@@ -2,13 +2,18 @@
 
 require('../app/functions.php');
 
-$message = trim(filter_input(INPUT_POST, 'message'));
-$message = $message !== '' ? $message : '...';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $message = trim(filter_input(INPUT_POST, 'message'));
+    $message = $message !== '' ? $message : '...';
+    
+    $filename = '../app/messages.txt';
+    $fp = fopen($filename, 'a');
+    fwrite($fp, $message . "\n");
+    fclose($fp);
+} else {
+    exit('Invaild Request');
+}
 
-$filename = '../app/messages.txt';
-$fp = fopen($filename, 'a');
-fwrite($fp, $message . "\n");
-fclose($fp);
 
 include('../app/_parts/_header.php');
 
